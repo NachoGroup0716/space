9dfa306c35dd478c5572d9cdfb4ab184616cb5ae
NACHOCHEESE <NACHOCHEESE@192.168.160.1>
2026-03-18 18:15:27 +0900

    main 메소드 추가

--- custom-strategy-adaptor-pj/src/com/file/cleaner/Runner.java
+++ custom-strategy-adaptor-pj/src/com/file/cleaner/Runner.java
@@ -1,9 +1,18 @@
 package com.file.cleaner;
 
+import com.file.cleaner.data.CleanerInterfaceInfo;
+import com.file.cleaner.service.CleanerService;
+
 public class Runner {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		CleanerInterfaceInfo info = new CleanerInterfaceInfo();
+		info.setSearchPaths("/test/test/test/test/@{yyyy}/@{mm}/temp/@{dd}");
+		info.setSearchRules("(IS_FILE and CREATION_DAYS_AGE > 60) or (IS_DIRECTORY and NAME_DAYS_AGE > 60)");
+		info.afterPropertiesSet();
 		
+		CleanerService service = new CleanerService();
+		service.onSignal(info);
 	}
 
 }
